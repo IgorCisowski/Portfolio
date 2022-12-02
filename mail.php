88c00fee705e7466906c95b91280e16cd42eb4ee
IgorCisowski <igorcisowski2098@gmail.com>
2022-12-02 20:59:23 +0100

    major changes

--- mail.php
+++ mail.php
@@ -1,16 +1,17 @@
 <?php
 
-if (isset($_POST['submit'])) {
+if(isset($_POST['email']) && $_POST["email"] != '') {
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$mailTo = "igorcisowski2098@gmail.com";
+$to = "igorcisowski2098@gmail.com";
 $headers = "From: ".$email
 $txt = "You have recived an e-mail from ".$name.".\n\n".$message;
 
 mail($mailTo, $message, $headers);
 header("Location: index.php?maildend");
-}
 
+}
 ?>
